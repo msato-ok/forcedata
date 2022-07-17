0a72621588450de5fb5a7798f56be2c007d438c3
佐藤円 <msato@altus5.com>
2022-07-17 07:32:55 +0000

    fix: サンプルの力指向グラフのhtmlを修正

--- examples/force-data.js
+++ examples/force-data.js
@@ -1,23 +1,5 @@
 var forceData = [
   {
-    "dataId": "Test01Device1",
-    "inheritDataId": "",
-    "properties": [
-      "Test01AndroidOrIos1",
-      "Test01AndroidOrIos2"
-    ]
-  },
-  {
-    "dataId": "Test01HairStyle1",
-    "inheritDataId": "",
-    "properties": []
-  },
-  {
-    "dataId": "Test01Friends1",
-    "inheritDataId": "",
-    "properties": []
-  },
-  {
     "dataId": "Test01Friends2",
     "inheritDataId": "",
     "properties": []
@@ -33,7 +15,64 @@ var forceData = [
     ]
   },
   {
+    "dataId": "Test05Base1",
+    "inheritDataId": "",
+    "properties": [
+      "Test05Device1"
+    ]
+  },
+  {
+    "dataId": "Test02Base1",
+    "inheritDataId": "Test01Base1",
+    "properties": [
+      "Test02Friends2"
+    ]
+  },
+  {
+    "dataId": "Test01Ios1",
+    "inheritDataId": "",
+    "properties": []
+  },
+  {
+    "dataId": "Test01HairStyle1",
+    "inheritDataId": "",
+    "properties": []
+  },
+  {
+    "dataId": "Test01Friends1",
+    "inheritDataId": "",
+    "properties": []
+  },
+  {
     "dataId": "Test04Friends3",
+    "inheritDataId": "",
+    "properties": []
+  },
+  {
+    "dataId": "Test01Device1",
+    "inheritDataId": "",
+    "properties": [
+      "Test01Android1",
+      "Test01Ios1"
+    ]
+  },
+  {
+    "dataId": "Test05Device1",
+    "inheritDataId": "",
+    "properties": []
+  },
+  {
+    "dataId": "Test02Friends2",
+    "inheritDataId": "Test01Friends2",
+    "properties": []
+  },
+  {
+    "dataId": "Test03Base1",
+    "inheritDataId": "Test02Base1",
+    "properties": []
+  },
+  {
+    "dataId": "Test01Android1",
     "inheritDataId": "",
     "properties": []
   },
@@ -47,12 +86,7 @@ var forceData = [
     ]
   },
   {
-    "dataId": "Test01AndroidOrIos1",
-    "inheritDataId": "",
-    "properties": []
-  },
-  {
-    "dataId": "Test01AndroidOrIos2",
+    "dataId": "Test06Base1",
     "inheritDataId": "",
     "properties": []
   }
